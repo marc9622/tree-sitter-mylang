849b909cb05dce8a9bb97a1d39aab05d75cd872f
marc9622 <maaw@itu.dk>
2023-10-30 00:43:18 +0100

    Added highlighting query

---
+++ queries/highlights.scm
@@ -0,0 +1,108 @@
+
+(line_comment) @comment
+(block_comment) @comment
+
+(int_literal) @number
+(dec_literal) @number
+
+(str_literal) @string
+
+(type_id) @type
+(value_id) @field
+[(macro_id) "macro"] @function.macro
+
+(func_call func_id: (value_id) @function)
+(param_value_decl decl_id: (value_id) @function (func_type))
+(func_decl decl_id: (value_id) @function)
+
+[
+"+"
+"-"
+"*"
+"/"
+"%"
+"!"
+"&"
+"^"
+"?"
+"::"
+"..=" "=.."
+"..<" "<.."
+"..>" ">.."
+"="
+"+=" "-="
+"*=" "/="
+"==" "!="
+"<=" ">="
+"<" ">" "<>"
+"->"
+] @operator
+
+[
+"."
+":"
+","
+";"
+] @punctuation.delimiter
+
+[
+"("
+")"
+"["
+"]"
+"{"
+"}"
+"|"
+] @punctuation.bracket
+
+[
+"true"
+"false"
+] @boolean
+
+[
+"uninit"
+"null"
+"void"
+void: ["(" ")"]
+] @constant.builtin
+
+[
+"import"
+"with"
+"as"
+"alias"
+"newtype"
+"struct"
+"union"
+"enum"
+"exposed"
+"trait"
+"impl"
+"for"
+"pub"
+"var"
+"def"
+"virt"
+"extern"
+"if"
+"switch"
+"at"
+"range"
+"in"
+"else"
+"is"
+"and"
+"or"
+"not"
+"move"
+"copy"
+"sizeof"
+"panic"
+"return"
+"break"
+"continue"
+"defer"
+"do"
+] @keyword
+
