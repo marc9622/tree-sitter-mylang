1e0e568d287ee1a33673d95cbec050eb1919a636
marc9622 <maaw@itu.dk>
2023-12-25 05:41:31 +0100

    Context and Macro queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -10,7 +10,7 @@
 (type_id) @type
 (value_id) @variable
 (member_value member: (value_id) @property)
-
+(macro_id) @macro
 (func_call func_id: (value_id) @function)
 (param_value_decl decl_id: (value_id) @function (func_type))
 (func_decl decl_id: (value_id) @function)
@@ -86,6 +86,10 @@ void: ["(" ")"]
 "extern"
 "intern"
 "context"
+"pure"
+"nopanic"
+"base"
+"async"
 "if"
 "switch"
 "in"
